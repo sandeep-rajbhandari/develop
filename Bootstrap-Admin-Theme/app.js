4740cb1a761085d3ffd5f68b3f61a9a75949b07f
srajbhandari <srajbhandari@deerwalk.com>
2014-10-10 18:07:22 +0545

    sa

---
+++ Bootstrap-Admin-Theme/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by srajbhandari on 10/10/14.
+ */
+var app=angular.module('appStore',[]);
+app.controller('clusterName',function($scope){
+$scope.clusterList=[
+    {name:'cluster1'},
+    {name:'cluster2'}
+
+]
+
+})
+
